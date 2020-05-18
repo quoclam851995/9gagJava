dcd48a94f60a7472bc4ec56971e64a2e979de73f
quoclam851995 <quoclam851995@gmail.com>
2020-05-18 16:47:53 +0700

    handle auth

--- src/main/java/com/morsoftware/api/AuthApi.java
+++ src/main/java/com/morsoftware/api/AuthApi.java
@@ -1,11 +1,15 @@
 package com.morsoftware.api;
 
 import com.morsoftware.authentication.form.request.LoginForm;
+import com.morsoftware.authentication.form.request.SignupForm;
 import com.morsoftware.authentication.form.response.JwtReponse;
 import com.morsoftware.authentication.jwt.JwtProvider;
+import com.morsoftware.entity.RoleEntity;
+import com.morsoftware.entity.UserEntity;
 import com.morsoftware.repository.RoleRepository;
 import com.morsoftware.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,6 +19,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.Set;
 
 @CrossOrigin
 @RestController
@@ -39,5 +45,18 @@ public class AuthApi {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = jwtProvider.generateJwtToken(authentication);
         return ResponseEntity.ok(new JwtReponse(jwt));
+    }
+    @PostMapping("/signup")
+    public ResponseEntity<String> registerUser(@Valid @RequestBody SignupForm signupRequest){
+        if (userRepository.existsByUserName(signupRequest.getUsername())){
+            return new ResponseEntity<String>("Fail -> Username has been available", HttpStatus.BAD_REQUEST);
+        }
+
+        if (userRepository.existsByEmail(signupRequest.getEmail())){
+            return new ResponseEntity<String>("Fail -> Email has been registered", HttpStatus.BAD_REQUEST);
+        }
+
+        UserEntity userEntity = new UserEntity(signupRequest.getFullname(), signupRequest.getUsername(),
+                signupRequest.getEmail(), passwordEncoder.encode(signupRequest.getPassword()));
     }
 }
